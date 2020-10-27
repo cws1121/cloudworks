c118af7d2ed5e00989c59b9cbb3d61380aad3f38
nemanja <nemanja.stancic.91@gmail.com>
2020-10-27 11:54:20 +0100

    fix ingest urls

--- django_backend/rdt/api/urls.py
+++ django_backend/rdt/api/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from rdt.api import views
 
 urlpatterns = [
-    url(r'^ingest/(?P<dsn>\w{1,50})/test_session/(?P<guid>\w{1,50})/media/(?P<media_id>\w{1,50})/', views.IngestMedia.as_view()),
-    url(r'^ingest/(?P<dsn>\w{1,50})/test_session/(?P<guid>\w{1,50})/', views.IngestTestSession.as_view()),
+    url(r'^ingest/(?P<dsn>[0-9A-Za-z_\-]+)/test_session/(?P<guid>[0-9A-Za-z_\-]+)/media/(?P<media_id>[0-9A-Za-z_\-]+)/', views.IngestMedia.as_view()),
+    url(r'^ingest/(?P<dsn>[0-9A-Za-z_\-]+)/test_session/(?P<guid>[0-9A-Za-z_\-]+)/', views.IngestTestSession.as_view()),
 ]
