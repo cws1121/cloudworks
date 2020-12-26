fada5ded02c2a3742b30093a7640ff35b59cb366
nemanja <nemanja.stancic.91@gmail.com>
2020-12-26 22:48:37 +0100

    django config, onetime setup added

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -12,6 +12,22 @@ def production():
 
 
 @task
+def onetime_setup_root():
+    env.user = 'root'
+    run('apt-get update')
+    run('apt-get install npm nodejs-legacy')
+    run('pip install --upgrade pip')
+    run('pip install supervisor==3.3.3')
+    run('echo_supervisord_conf > /etc/supervisord.conf')
+    run(r"printf '[include]\nfiles = /home/web/www/cloudworks/deploy/*.conf\n' >> /etc/supervisord.conf")
+    run("grep '^web.*NOPASSWD: /usr/local/bin/supervisorctl' /etc/sudoers.d/sudo_commands "
+        "|| echo 'web     ALL = (ALL) NOPASSWD: /usr/local/bin/supervisorctl*' >> /etc/sudoers.d/sudo_commands")
+    run("supervisord -c /etc/supervisord.conf"
+        "|| echo 'restarting supervisorctl' && kill $(ps x | grep [/]usr/local/bin/supervisord | cut -f1 -d' ')"
+        "&& supervisord -c /etc/supervisord.conf")
+
+
+@task
 def deploy():
     with settings(sudo_user=env.web_user):
         with shell_env(HOME='/home/{}/'.format(env.web_user)):
@@ -22,8 +38,8 @@ def deploy():
                 sudo('python_env/bin/pip install -r requirements/prod-requirements.txt -r requirements/requirements.txt ')
                 # sudo('python_env/bin/python manage.py collectstatic --no-input')
                 sudo('python_env/bin/python manage.py migrate --no-input')
-                # sudo('sudo ./bin/gunicorn_start')
-
+                sudo('sudo ./bin/gunicorn_start')
+# sudo('sudo supervisorctl restart all')
 
 @task
 def deploy_and_build():
